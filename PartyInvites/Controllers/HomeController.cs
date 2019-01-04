af91238b825751e0802d49f35646f30c46011807
momenislc <aymomeni@gmail.com>
2019-01-04 13:19:07 -0700

    initial commit of PartyInvites with boiler plate ASP.Net project

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = System.DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        
+    }
+}
